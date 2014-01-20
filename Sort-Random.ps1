f90658273184a42b5cee60fa5a181e7a33a1138b
The Powershell Bear <chrisrlowery@gmail.com>
2014-01-19 21:52:02 -0800

    Added handling for odd number of users
    Added logic to handle an odd number of participants and request which
    one to give an extra pairing to; also fixed a bug that was causing
    pairings to not remove participants from $list2 as they got assigned.
    
    Discovered an unfixed new bug, that if the person selected to get two
    partners gets both copies of their name added to $list2, both will be
    removed when they are paired the first time causing an unmatched person
    at the end.  Setting them as Primary (which will eliminate the
    possibility that they will be matched to themselves) will fix this, but
    it should be corrected any way.

--- Sort-Random.ps1
+++ Sort-Random.ps1
@@ -1,51 +1,86 @@
 ﻿#Initial setup for the name pair randomizer
 
-$testnames = "Syed, Kim, Sam, Hazem, Pilar, Terry, Amy, Greg, Pamela, Julie, David, Robert, Shai, Ann, Mason, Sharon"
+$testnames = "Syed, Kim, Sam, Hazem, Pilar, Terry, Amy, Greg, Pamela, Julie, David, Robert, Shai, Ann, Mason, Sharon, Dude"
 $testnames = ($testnames.split(',')).trim()
+
+### BUG: If a duplicate name is in the second list, both will get removed when one is matched. 
+###      This causes a problem particularly with how the odd-number handler is written now, but will be masked when Primary handling is introduced.
+###      This should preferrably be corrected in spite of that, however; perhaps by creating a separate random index for each person.
 
 function Sort-Random {
     param([string[]]$names)
 
-    if ($names.count % 2 -eq 1) { <# Odd number, how do we handle this? #> }
+    $continue = $True
 
-    $GetRandom = $true
-    while ($GetRandom) {
-        $MasterList = foreach ($name in $names) {
-            $random = Get-Random -Minimum 100000 -Maximum 150000
-            if ($false) {
-                #for Primes, not implemented yet
-                #Reduce random number by 100000 to ensure it's at the top
-                $random = $random - 100000
+    if ($names.count % 2 -eq 1) { 
+        #Note: to make this work, we should mark the person getting a second partner as a Primary.
+        #Since there's no mechanism for that yet, we'll just hope they don't get themselves for now.
+        Write-Warning "The number of people in your list is $($names.count), which is an odd number.`nPlease select a person to get two partners:"
+        $i = 1
+        $PossibleSelections = @()
+        foreach ($name in $names) {
+            Write-Output "$($i): $name"
+            $PossibleSelections += "$i"
+            $i ++
+        }
+        Write-Output "0: Cancel and try again"
+        $selection = Read-Host -Prompt "Please select"
+        if ($selection -eq "0") {
+            Write-Warning "Cancelling random selection."
+            $continue = $false
+        }
+        elseif ($selection -notin $PossibleSelections) {
+            #should make this have it try again; being lazy for now
+            Write-Warning "That was not a recoginzed selection; please try again."
+            $continue = $false
+        }
+        else {
+            $extra = $names[[int]$selection - 1]
+            Write-Warning "$extra selected to take multiple partners."
+            $names = $names + $extra
+        }
+    }
+
+    if ($continue) {
+        $GetRandom = $true
+        while ($GetRandom) {
+            $MasterList = foreach ($name in $names) {
+                $random = Get-Random -Minimum 100000 -Maximum 150000
+                if ($false) {
+                    #for Primes, not implemented yet
+                    #Reduce random number by 100000 to ensure it's at the top
+                    $random = $random - 100000
+                }
+
+                $props = @{'Name' = $name;
+                           'Random' = $random}
+
+                $obj = New-Object -TypeName PSObject -Property $props
+                Write-Output $obj
             }
+        
+            #Make sure all random numbers are unique; otherwise, re-randomize the list
+            if ( ($MasterList | Group -Property Random).count -eq $MasterList.count ) {
+                $GetRandom = $False
+            }
+        }
 
-            $props = @{'Name' = $name;
-                       'Random' = $random}
+        $MasterList = $MasterList | Sort -Property Random
+        $list1 = $MasterList[0..($names.count/2-1)]
+        $list2 = $MasterList[($names.count/2)..($names.count)]
+
+        $output = foreach ($name in $list1) {
+            #get list of exceptions here
+            $partner = $list2 | Get-Random # | where name -notin $exceptions
+            $list2 = $list2 | where Name -ne $partner.name
+            $props = @{'Name' = $name.name;
+                       'Parter' = $partner.name}
 
             $obj = New-Object -TypeName PSObject -Property $props
             Write-Output $obj
         }
-        
-        #Make sure all random numbers are unique; otherwise, re-randomize the list
-        if ( ($MasterList | Group -Property Random).count -eq $MasterList.count ) {
-            $GetRandom = $False
-        }
+        Write-Output $output
     }
-
-    $MasterList = $MasterList | Sort -Property Random
-    $list1 = $MasterList[0..($names.count/2-1)]
-    $list2 = $MasterList[($names.count/2)..($names.count)]
-
-    $output = foreach ($name in $list1) {
-        #get list of exceptions here
-        $partner = $list2 | Get-Random # | where name -notin $exceptions
-        $list2 = $list2 | where Name -ne $partner
-        $props = @{'Name' = $name.name;
-                   'Parter' = $partner.name}
-
-        $obj = New-Object -TypeName PSObject -Property $props
-        Write-Output $obj
-    }
-    Write-Output $output
 }
 
 Sort-Random $testnames
